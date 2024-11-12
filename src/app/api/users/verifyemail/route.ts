b29ed711b85ec637d96acfac2f31a809a8a2f27b
ujjwal-dahal <toppelp@gmail.com>
2024-11-12 15:28:47 +0545

    Verify Email Route is added

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,42 @@
+import DatabaseConnection from "@/dbConnection/dbConnection";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+
+DatabaseConnection();
+
+export async function POST(request : NextRequest){
+  try {
+
+    const reqBody = await request.json();
+
+    const { token } = reqBody;
+    console.log(token)
+
+    const user = await User.findOne({
+      verifyToken : token,
+      verifyTokenExpiry : {$gt : Date.now()}
+    });
+
+    if(!user){
+      return NextResponse.json({error : "Invalid Token"},{status : 400})
+    }
+
+    console.log(user)
+
+    user.isVerified = true; //eddi user return bhayo bhane isVerified value true huncha
+    user.verifyToken = undefined; //abo use verify bhaesi verifyToken haru hatako
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json({
+      message : "Email verified successfully",
+      success : true,
+    },{status : 200})
+
+    
+  } catch (error : any) {
+    return NextResponse.json({error : error.message},{status : 500})
+    
+  }
+}
