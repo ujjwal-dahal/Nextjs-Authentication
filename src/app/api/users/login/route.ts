217728e56614df8b6256002421eecb14a8e1582b
ujjwal-dahal <toppelp@gmail.com>
2024-11-12 16:03:59 +0545

    Login Route is applied

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,7 +1,63 @@
-import {NextResponse , NextRequest} from "next/server"
+import { NextResponse, NextRequest } from "next/server";
+import DatabaseConnection from "@/dbConnection/dbConnection";
+import bcrypt from "bcryptjs";
+import User from "@/models/userModel";
+import jwt from "jsonwebtoken";
 
-export default function POST(request : NextRequest){
-  
+// Establish the database connection
+DatabaseConnection();
 
+export async function POST(request: NextRequest) {
+  try {
+    // Parse JSON data from the request body
+    const reqBody = await request.json();
+    const {email, password } = reqBody;
+    console.log(reqBody)
 
+    // Find the user by email and username
+    const user = await User.findOne({ email});
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not Exist" },
+        { status: 401 }
+      );
+    }
+
+    // Verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        { message: "Invalid Credentials" },
+        { status: 401 }
+      );
+    }
+
+    const tokenData = {
+      id : user._id,
+      username : user.username,
+      email : user.email
+    }
+
+    const jwtToken = await jwt.sign(tokenData , process.env.TOKEN_SECRET! , {expiresIn : '1d'})
+
+    // Successful login response
+    const response = NextResponse.json(
+      { message: "Successfully Logged In", success: true },
+      { status: 200 }
+    );
+
+    //abo yo response ma hamile cookies pathauna sakchum
+
+    response.cookies.set("token" , jwtToken, {
+      httpOnly : true
+    })
+
+    return response ; //abo response return bhayo
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
 }
