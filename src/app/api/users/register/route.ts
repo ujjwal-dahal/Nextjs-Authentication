8a59ba6b2a99348fa540b3e1db09ab87fc2077b1
ujjwal-dahal <toppelp@gmail.com>
2024-11-12 09:54:27 +0545

    Send Mail Setup and Ready to Test register api

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -12,6 +12,8 @@ import garda "@" le root directory i.e src bujincha
 import DatabaseConnection from "@/dbConnection/dbConnection";
 import User from "@/models/userModel";
 import {NextRequest , NextResponse} from "next/server";
+import bcrypt from "bcryptjs";
+import { sendMail } from "@/helpers/emailSend";
 
 DatabaseConnection(); //Abo Database Connect bhayo
 
@@ -19,6 +21,62 @@ DatabaseConnection(); //Abo Database Connect bhayo
 export async function POST(request : NextRequest){
 
   try {
+
+    const reqBody = await request.json();  //JSON Response aaucha i.e User le Enter gareko data
+
+    const {username , email , password} = reqBody;
+
+    //Validation
+
+    console.log(reqBody);
+
+    //User Lai Regester Garne
+
+    //Check garne user alrady exist garcha ki gardaina
+
+    let existUser : any = await User.findOne({email}); //Email already chaki chaina
+
+    if(existUser){
+      return NextResponse.json({error : "User Already Exist"},{status : 400});
+    }
+    
+
+    //Abo User exist gareko chaina bhane Register garne database ma
+    //Tyo Bhanda aagadi Password lai hash garne
+
+    const hashedPassword = await bcrypt.hash(password , 10); //hash password
+
+    //await User.create({
+    //   username , 
+    //   email , 
+    //   password : hashedPassword
+    // }); //Esle new user create garcha ani Database ma immediately save garcha
+
+    const newUser = new User({
+      username ,
+      email,
+      password : hashedPassword
+    }); //Esle new user create garcha tara database ma save gardaina
+
+    const savedUser = await newUser.save(); //database sanga interact gareko le await use gareko
+
+    console.log(savedUser);
+
+    //Abo Send Mail Verification after Saving user in database
+
+    await sendMail({email : email , emailType : "VERIFY" , userId : savedUser._id});
+
+    return NextResponse.json({
+      message : "User Register Successfully",
+      success : true,
+      savedUser })
+
+
+
+
+
+
+
 
     
   } catch (error : any) {
--- src/helpers/emailSend.ts
+++ src/helpers/emailSend.ts
@@ -1,16 +1,42 @@
+import User from "@/models/userModel";
 import nodemailer from "nodemailer";
+import bcrypt from "bcryptjs";
 
 
 export const sendMail = async ({email , emailType , userId}:any)=>{
-
+  let hashedToken = await bcrypt.hash(userId.toString(), 10);
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 465,
-      secure: false, // true for port 465, false for other ports
+    if(emailType === "VERIFY"){
+      //abo Verify Token and Expiry Date chai Database ma halne
+
+      let hashedToken = await bcrypt.hash(userId.toString(), 10);
+
+      await User.findByIdAndUpdate(userId,
+        {
+          verifyToken : hashedToken,
+          verifyTokenExpiry : Date.now() + 3600000
+        }
+      ) //id ko help le database ma user find garcha ani Update garcha 
+    }
+    else if(emailType === "RESET"){
+
+      await User.findByIdAndUpdate(userId,
+        {
+          forgotPasswordToken : hashedToken,
+          forgotPasswordTokenExpiry : Date.now() + 3600000
+        }
+      )
+    }
+
+
+
+    
+    const transport = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: Number(process.env.MAILTRAP_PORT),
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
       },
     });
 
@@ -18,10 +44,21 @@ export const sendMail = async ({email , emailType , userId}:any)=>{
       from: "toppelp@gmail.com", // sender address
       to: email, // list of receivers
       subject: emailType==="VERIFY" ? "Verify Your Email" :"Reset Your Password", // Subject line
-      html: "<b>Hello world?</b>", // html body
+      html: emailType=== "VERIFY" ?  
+      
+      `<p>
+      Click <a href="${ process.env.DOMAIN }/verifyemail?token=${hashedToken}">Here</a> to verify your email
+      or Copy & Paste the link below in your browser.
+      <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+      </p>`
+      : 
+      `Click <a href="${ process.env.DOMAIN }/resetpassword?token=${hashedToken}">Here</a> to reset your password"
+      or Copy & Paste the link below in your browser.
+      <br> ${process.env.DOMAIN}/resetpassword?token=${hashedToken}
+      </p>` ,
     }
 
-    const mailResponse = await transporter.sendMail(mailOptions)
+    const mailResponse = await transport.sendMail(mailOptions)
 
     return mailResponse;
     
