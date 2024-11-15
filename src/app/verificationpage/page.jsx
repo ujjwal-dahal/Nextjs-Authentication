fb34fbe123978523b9e6d2d96e7a24e24389bf8c
ujjwal-dahal <toppelp@gmail.com>
2024-11-15 22:13:23 +0545

    Verification page error has fixed

--- src/app/verificationpage/page.jsx
+++ src/app/verificationpage/page.jsx
@@ -4,7 +4,7 @@ export default function VerificationPage() {
   return (
     <div className="verification-page">
       <h1>Please Check Your Email For Verification</h1>
-      <p>If you don't see it, please check your spam folder or request another email.</p>
+      <p>If you don&apos;t see it, please check your spam folder or request another email.</p>
       <button className="check-email-btn">Resend Email</button>
     </div>
   );
